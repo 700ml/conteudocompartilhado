2e56e1ec6e7a9890e4c972e66797da4db03ec07a
Rafael Nat? <rafael.nathan.c@gmail.com>
2016-07-24 16:41:34 -0300

    base

---
+++ devop/sheets.js
@@ -0,0 +1,515 @@
+
+    /*for(var i = 0; i < 39; i++) {
+        images.push({
+            Image: {
+                xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+                Url: i+"/img/GeneratedImages/dzc_output_files/",
+                tileSources: i+"/img/GeneratedImages/dzc_output.xml"
+            }
+        });
+    }*/
+    var images2 = [{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "3/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "3875"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "4/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "739"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "5/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "819"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "7/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "661"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "8/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "803"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "9/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "1300"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "10/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "1439"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "11/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "1228"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "12/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "675"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "13/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "576"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "14/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "576"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "15/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "576"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "16/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "576"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "17/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "576"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "18/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "576"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "19/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "675"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "20/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "613"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "21/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "587"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "22/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "576"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "23/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "576"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "24/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "576"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "25/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "1393"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "26/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "576"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "27/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "4608"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "28/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "835"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "29/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "643"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "30/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "776"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "31/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "757"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "32/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "670"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "33/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "1262"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "34/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "1141"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "35a/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "273",
+                Height: "486"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "35b/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "273",
+                Height: "486"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "35c/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "273",
+                Height: "486"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "35d/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "273",
+                Height: "486"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "35e/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "273",
+                Height: "486"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "35f/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "273",
+                Height: "486"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "36/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "669"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "36a/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "864",
+                Height: "486"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "37/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "825"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "38/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "707"
+            }
+        }
+    },{
+        Image: {
+            xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+            Url: "39/img/GeneratedImages/dzc_output_files/",
+            Format: "jpg",
+            Overlap: "2",
+            TileSize: "256",
+            Size: {
+                Width:  "1060",
+                Height: "581"
+            }
+        }
+    }];
